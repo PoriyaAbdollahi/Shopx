fb15b98da6b63aad6bed8eda1b28087b5236e967
poriyaabdollahi <poriyaa58@gmail.com>
2021-03-24 19:58:33 +0430

    Sign Up XMl Is Completed

---
+++ app/src/main/java/com/example/shopx/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.shopx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
