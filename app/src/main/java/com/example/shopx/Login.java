110b13244b6faa521ca636a4e0fdfcb131a1ee0e
poriyaabdollahi <poriyaa58@gmail.com>
2021-03-25 15:12:56 +0430

    Login Xml is Completed

---
+++ app/src/main/java/com/example/shopx/Login.java
@@ -0,0 +1,15 @@
+package com.example.shopx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
