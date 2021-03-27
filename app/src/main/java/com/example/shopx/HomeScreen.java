ce780b8cc05617243d9c1010e744d53822425f70
poriyaabdollahi <poriyaa58@gmail.com>
2021-03-27 20:51:35 +0430

    HomeScreen: Constraint and Ui 50%

--- app/src/main/java/com/example/shopx/HomeScreen.java
+++ app/src/main/java/com/example/shopx/HomeScreen.java
@@ -1,16 +1,17 @@
 package com.example.shopx;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class InfoGetter extends AppCompatActivity {
+
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_getter);
-
+        setContentView(R.layout.activity_home_screen);
 
 
     }
